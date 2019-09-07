6e9eab94ba14853eafc3eeab0a356c985fef30ac
liyingpeng <liyingpeng@bytedance.com>
2019-09-07 15:50:59 +0800

    add python

---
+++ c++test/c++test/lru.cpp
@@ -0,0 +1,9 @@
+//
+//  lru.cpp
+//  c++test
+//
+//  Created by 李应鹏 on 2019/6/23.
+//  Copyright © 2019 李应鹏. All rights reserved.
+//
+
+#include "lru.hpp"
---
+++ c++test/c++test/lru.hpp
@@ -0,0 +1,14 @@
+//
+//  lru.hpp
+//  c++test
+//
+//  Created by 李应鹏 on 2019/6/23.
+//  Copyright © 2019 李应鹏. All rights reserved.
+//
+
+#ifndef lru_hpp
+#define lru_hpp
+
+#include <stdio.h>
+
+#endif /* lru_hpp */
